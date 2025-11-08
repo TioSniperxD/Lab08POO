c81c7f2862892c20d73741764f3febbf487ab6d3
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-08 04:32:08 -0500

    Subir clase Cliente.java

---
+++ Clases/Cliente.java
@@ -0,0 +1,37 @@
+package Clases;
+
+public class Cliente extends Persona {
+    //ATRIBUTOS
+    private Cuenta cuenta; 
+
+    //Constructor sobrecargado
+    public Cliente (String nombre, String id, String direccion, Cuenta cuenta){
+        super(nombre, id, direccion);
+        this.cuenta = cuenta;
+    }
+
+    //Constructor para registrar un nuevo cliente
+    public Cliente (String nombre, String id, String direccion) {
+        super(nombre, id, direccion);
+        this.cuenta = null; // Un cliente puede empezar sin cuenta
+    }
+
+    //MÉTODOS (getters y setters)
+    public Cuenta getCuenta() {
+        return this.cuenta;
+    }
+    public void setCuenta (Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    //Mostrar información
+    @Override
+    public void mostrarInformacion () {
+        super.mostrarInformacion();
+        if (cuenta != null) {
+            System.out.println("Cuenta del cliente: " + cuenta);
+        } else {
+            System.out.println("El cliente no tiene cuenta asignada.");
+        }
+    }
+}
