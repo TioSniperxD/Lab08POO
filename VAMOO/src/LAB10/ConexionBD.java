d55d6f5a0b8f148490608352119cc33be98609ed
Aleyda <aquispehach@unsa.edu.pe>
2025-12-08 22:33:06 -0500

    feat: Subir conexión a base de datos MySQL

--- VAMOO/src/LAB10/ConexionBD.java
+++ VAMOO/src/LAB10/ConexionBD.java
@@ -10,7 +10,6 @@ package LAB10;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/banco";
